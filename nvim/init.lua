ae974584963d34863f0347963a3417ae63ab6b18
Telman Rzaev <rzaev.telman.tarik@gmail.com>
2023-09-23 09:48:00 +0300

    feat: nvim config

---
+++ nvim/init.lua
@@ -0,0 +1,13 @@
+-- plugins
+require("plugins.lazy")
+require("plugins.treesitter")
+require("plugins.kanagawa")
+require("plugins.telescope")
+-- lsp-plugins
+require("plugins.lsp.null-ls")
+require("plugins.lsp.mason")
+require("plugins.lsp.lspconfig")
+require("plugins.nvim-cmp")
+
+require("options")
+require("keymaps")
